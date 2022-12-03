2e95733df2a556cf052a3ab3b9ba2b758ff323cb
Ryan DeVault <ctrlvaultdel51@gmail.com>
2022-12-02 19:38:34 -0600

    Working on import issue

--- 2022/day1/rawInput.mjs
+++ 2022/day1/rawInput.mjs
@@ -1,8 +1,5 @@
-// https://adventofcode.com/2022/day/1
-
-const solution1 = () => {
-    // Original input (string)
-    const input = `5557
+export const rawInput = `
+5557
 6663
 3952
 4750
@@ -2239,34 +2236,3 @@ const solution1 = () => {
 3605
 4191
 1482`
-
-    // Will hold array of each elf's total calories
-    const elfArr = [];
-
-    let elfIdx = 0;
-    let totalCalories = 0;
-
-    input
-        .split("\n")
-        .forEach(amount => {
-            // When value is empty, start on next elf
-            if (amount === '') {
-                elfArr[elfIdx] = totalCalories;
-                elfIdx++;
-                totalCalories = 0;
-            }
-            // Otherwise, keep adding to current elf's calories
-            else totalCalories += parseInt(amount);
-        })
-
-    // Sort from elves with highest calories to lowest
-    elfArr.sort((a, b) => b - a);
-
-    // Answer 1
-    console.log(`Answer #1: ${elfArr[0]}`)
-    
-    // Answer 2
-    console.log(`Answer #2: ${elfArr[0] + elfArr[1] + elfArr[2]}`)
-};
-
-solution1();
---
+++ 2022/day1/solution1.mjs
@@ -0,0 +1,36 @@
+// https://adventofcode.com/2022/day/1
+import rawInput from "./rawInput.mjs";
+
+const solution1 = () => {
+
+    console.log(rawInput)
+    // Will hold array of each elf's total calories
+    const elfArr = [];
+
+    let elfIdx = 0;
+    let totalCalories = 0;
+
+    rawInput
+    .split("\n")
+    .forEach(amount => {
+        // When value is empty, start on next elf
+        if (amount === '') {
+            elfArr[elfIdx] = totalCalories;
+            elfIdx++;
+            totalCalories = 0;
+        }
+        // Otherwise, keep adding to current elf's calories
+        else totalCalories += parseInt(amount);
+    })
+
+    // Sort from elves with highest calories to lowest
+    elfArr.sort((a, b) => b - a);
+
+    // Answer 1
+    console.log(`Answer #1: ${elfArr[0]}`)
+    
+    // Answer 2
+    console.log(`Answer #2: ${elfArr[0] + elfArr[1] + elfArr[2]}`)
+};
+
+solution1();
