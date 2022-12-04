e4ec2cb9e985464e970759e994a30a2268b3b07d
Ryan DeVault <69012106+CtrlVaultDel@users.noreply.github.com>
2022-12-04 06:56:11 -0600

    Initial push for day 5

---
+++ 2022/day5/solution.mjs
@@ -0,0 +1,11 @@
+// 
+
+import { input } from "./input.mjs";
+import { solutionLabel } from "../../Helpers/solutionLabel.mjs";
+
+const solution = () => {
+    const inputArr = input
+        .split("\n");
+}
+
+solutionLabel(solution, 5);
